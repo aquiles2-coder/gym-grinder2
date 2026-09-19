443fc09e4d27b94695eb757198133d178e859087
Jorge Panagopoulos <spatra.jorge300@gmail.com>
2026-09-19 22:14:35 +0100

    Update Firebase configuration with new app and measurement IDs

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDfvdjCBEd0pGvvMDmNnL73WITBUZMN0-k",
   authDomain: "gym-grinder2.firebaseapp.com",
+  databaseURL: "https://gym-grinder2-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "gym-grinder2",
   storageBucket: "gym-grinder2.firebasestorage.app",
   messagingSenderId: "11495272962",
-  appId: "1:11495272962:web:c9ae6d1cb425da7ddd2d1b",
-  measurementId: "G-MT6H4MVTLV"
+  appId: "1:11495272962:web:3938a3d0b6868a7cdd2d1b",
+  measurementId: "G-2DCHY1K3H9"
 };
 
 firebase.initializeApp(firebaseConfig);
